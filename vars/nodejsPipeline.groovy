d91d5db6851959ad43ad42bbfd60add89e4b4803
srikanthg <skanth306@gmail.com>
2025-01-18 19:15:45 +0530

    shared library

--- vars/nodejsPipeline.groovy
+++ vars/nodejsPipeline.groovy
@@ -22,7 +22,7 @@ pipeline {
         account_id = '608782704145'
         project = 'expense'
         environment = 'dev'
-        component = ${config.component}
+        component = "${config.component}"
         customImage = ''
         SONAR_HOME = tool 'sonar-6.2' // scanner configuration servername and scanner name both should be same
         //PATH = "/usr/bin:${env.PATH}"  // Force the pipeline to use /usr/bin/git
