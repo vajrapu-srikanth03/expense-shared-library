b99c819f14c81e29e06a893f0efeda9f08cbdb91
srikanthg <skanth306@gmail.com>
2025-01-18 19:10:32 +0530

    shared library

--- vars/nodejsPipeline.groovy
+++ vars/nodejsPipeline.groovy
@@ -8,19 +8,21 @@ pipeline {
         git 'git'  // Git tool
         snyk 'snyk' // snyk tool
     }
-    options pipelineOptions()
+    options {
+        pipelineOptions()
+    } 
 
     environment {
         
-        DEBUG : 'true'
-        appVersion : ''  // Example version, replace with actual value
-        region : 'us-east-1'
-        account_id : '608782704145'
-        project : 'expense'
-        environment : 'dev'
-        component : ${config.component}
-        customImage : ''
-        SONAR_HOME= tool 'sonar-6.2' // scanner configuration servername and scanner name both should be same
+        DEBUG = 'true'
+        appVersion = ''  // Example version, replace with actual value
+        region = config.region ?: 'us-east-1'
+        account_id = '608782704145'
+        project = 'expense'
+        environment = 'dev'
+        component = ${config.component}
+        customImage = ''
+        SONAR_HOME = tool 'sonar-6.2' // scanner configuration servername and scanner name both should be same
         //PATH = "/usr/bin:${env.PATH}"  // Force the pipeline to use /usr/bin/git
     }
     
