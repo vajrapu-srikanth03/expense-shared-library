500aa325396c0e9125d1ac83a10563ac36674246
srikanthg <skanth306@gmail.com>
2025-01-22 12:13:53 +0530

    shared library

--- vars/common.groovy
+++ vars/common.groovy
@@ -10,129 +10,164 @@ def checkout(config) {
 }
 
 def readVersion(appType) {
-    if(appType=="nodejs"){
-        script {
-            def props = readJSON file: 'package.json'
-            appVersion = props.version
-            echo "App version: ${appVersion}"
+    stage(''Read the version'') {
+        if(appType=="nodejs"){
+            script {
+                def props = readJSON file: 'package.json'
+                appVersion = props.version
+                echo "App version: ${appVersion}"
+            }
         }
     }
+
 }
 
 def codeQuality(){
-    withSonarQubeEnv('sonar'){ //server name
-        sh '$SONAR_HOME/bin/sonar-scanner'
-        //generic scanner, it automatically understands the language and provide scan results
-    }
+    stage('SonarQube Code Analysis'){
+        withSonarQubeEnv('sonar'){ //server name
+            sh '$SONAR_HOME/bin/sonar-scanner'
+            //generic scanner, it automatically understands the language and provide scan results
+        }
+    }    
 }
 
 def qualityGate(){
-    timeout(time: 5, unit: 'MINUTES') {
-    waitForQualityGate abortPipeline: true, credentialsId: 'Sonar-token'
-    }
-}
-
-def installDependencies(appType) {
-    if(appType=="nodejs"){
-        sh 'npm install'
-    }
-}
-
-def dependencyCheck() {
-    dependencyCheck additionalArguments: '--scan ./ --disableYarnAudit --disableNodeAudit', odcInstallation: 'DP-Check'
-    dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
-}
-
-def snykCheck() {
-    snykSecurity(
-        organisation: 'vajrapu-srikanth03',
-        projectName: 'backend',
-        snykInstallation: 'snyk',
-        snykTokenId: 'snyk-token',
-        targetFile: 'package.json',
-    )
-}
-
-def trivyfsScan() { 
-    sh "trivy fs --format table -o trivy-fs-report.html ."
-}
-
-
-def npmbuild(appType) {
-    script {
-        if(appType=="nodejs"){
-            sh 'npm run build' // Build the Node.js project
+    stage('Quality Gate') {
+        timeout(time: 5, unit: 'MINUTES') {
+        waitForQualityGate abortPipeline: true, credentialsId: 'Sonar-token'
         }
     }
 }
 
 def build(appType){
-    if(appType=="nodejs"){
-        sh "zip -q -r ${JOB_BASE_NAME}.zip Dockerfile package.json TransactionService.js index.js schema/ -x '.git' -x '*.gitignore' -x '*.zip'"
-    }  
+    stage('Build') {
+        if(appType=="nodejs"){
+            sh "zip -q -r ${JOB_BASE_NAME}.zip Dockerfile package.json TransactionService.js index.js schema/ -x '.git' -x '*.gitignore' -x '*.zip'"
+        }
+    } 
 }
 
 def nexusUpload() {
-    script {
-        nexusArtifactUploader(
-            nexusVersion: 'nexus3',
-            protocol: 'http',
-            nexusUrl: pipelineGlobals.nexusURL(),
-            groupId: 'com.expense',
-            version: "${appVersion}",
-            repository: "${JOB_BASE_NAME}",
-            credentialsId: 'nexus-auth',
-            artifacts: [
-                [artifactId: "${JOB_BASE_NAME}",
-                classifier: '',
-                file: "${JOB_BASE_NAME}.zip",
-                type: 'zip']
-            ]
+    stage('Nexus Upload') {
+        script {
+            nexusArtifactUploader(
+                nexusVersion: 'nexus3',
+                protocol: 'http',
+                nexusUrl: pipelineGlobals.nexusURL(),
+                groupId: 'com.expense',
+                version: "${appVersion}",
+                repository: "${JOB_BASE_NAME}",
+                credentialsId: 'nexus-auth',
+                artifacts: [
+                    [artifactId: "${JOB_BASE_NAME}",
+                    classifier: '',
+                    file: "${JOB_BASE_NAME}.zip",
+                    type: 'zip']
+                ]
+            )
+        }
+    }
+}
+
+def installDependencies(appType) {
+    stage('Install Dependencies') {
+        if(appType=="nodejs"){
+            sh 'npm install'
+        }
+    }
+}
+
+def dependencyCheck() {
+    stage("OWASP Dependency Check"){
+        dependencyCheck additionalArguments: '--scan ./ --disableYarnAudit --disableNodeAudit', odcInstallation: 'DP-Check'
+        dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
+    }
+}
+
+def snykCheck() {
+    stage("Dependency Check using Snyk"){
+        snykSecurity(
+            organisation: 'vajrapu-srikanth03',
+            projectName: 'backend',
+            snykInstallation: 'snyk',
+            snykTokenId: 'snyk-token',
+            targetFile: 'package.json',
         )
     }
 }
 
+def trivyfsScan() {
+    stage("Trivy filesystem Scan"){
+        sh "trivy fs --format table -o trivy-fs-report.html ."
+    } 
+}
+
+
+def npmbuild(appType) {
+    stage('npm Build') {
+        script {
+            if(appType=="nodejs"){
+                sh 'npm run build' // Build the Node.js project
+            }
+        }
+    }
+}
+
+
+
 def dockerBuild(){
-    script {
-        customImage = docker.build("srikanthhg/$JOB_BASE_NAME:${appVersion}")
+    stage('Docker build') {
+        script {
+            customImage = docker.build("srikanthhg/$JOB_BASE_NAME:${appVersion}")
+        }
     }
 }
 
 def dockerScout(){
-    script {
-        withDockerRegistry(credentialsId: 'docker-auth', toolName: 'docker'){
-        sh "docker-scout cves srikanthhg/$JOB_BASE_NAME:${appVersion}"
-        
+    stage('Docker Scout Image'){
+        script {
+            withDockerRegistry(credentialsId: 'docker-auth', toolName: 'docker'){
+            sh "docker-scout cves srikanthhg/$JOB_BASE_NAME:${appVersion}"
+            
+            }
+            //--exit-code --only-severity critical,high
         }
-        //--exit-code --only-severity critical,high
     }
 }
 
 def trivyImageScan(){
-    sh "trivy image --format table srikanthhg/$JOB_BASE_NAME:${appVersion}"
+    stage('Image scan') {
+        sh "trivy image --format table srikanthhg/$JOB_BASE_NAME:${appVersion}"
+    }
 }
 
 def pushImagetoDockerhub(){
-    script {
-        withDockerRegistry(credentialsId: 'docker-auth', toolName: 'docker') {
-        customImage.push()
+    stage('push image to dockerhub') {
+        script {
+            withDockerRegistry(credentialsId: 'docker-auth', toolName: 'docker') {
+            customImage.push()
+            }
         }
     }
 }
 
 def pushImagetoAWSECR(){
-    script {
-        withAWS(region: 'us-east-1', credentials: 'aws-ecr') {
-            sh "aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${account_id}.dkr.ecr.us-east-1.amazonaws.com"
-            sh "docker tag srikanthhg/$JOB_BASE_NAME:${appVersion} ${account_id}.dkr.ecr.us-east-1.amazonaws.com/${project}/$JOB_BASE_NAME:${appVersion}"
-            sh "docker push ${account_id}.dkr.ecr.us-east-1.amazonaws.com/${project}/$JOB_BASE_NAME:${appVersion}"
-            sh "aws ecr describe-image-scan-findings --repository-name ${project}/$JOB_BASE_NAME --image-id imageTag=${appVersion}  --region ${region} "
-            def scanResults = sh(script: """ aws ecr describe-image-scan-findings --repository-name ${project}/$JOB_BASE_NAME --image-id imageTag=${appVersion}  --region ${region} --query "imageScanFindings.findings" """, returnStdout: true).trim()
+    stage('push image to AWS ECR') {
+        script {
+            withAWS(region: 'us-east-1', credentials: 'aws-ecr') {
+                sh "aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${account_id}.dkr.ecr.us-east-1.amazonaws.com"
+                sh "docker tag srikanthhg/$JOB_BASE_NAME:${appVersion} ${account_id}.dkr.ecr.us-east-1.amazonaws.com/${project}/$JOB_BASE_NAME:${appVersion}"
+                sh "docker push ${account_id}.dkr.ecr.us-east-1.amazonaws.com/${project}/$JOB_BASE_NAME:${appVersion}"
+                
+                sh "aws ecr describe-image-scan-findings --repository-name ${project}/$JOB_BASE_NAME --image-id imageTag=${appVersion}  --region ${region} "
+                
+                scanResults = sh(script: """ aws ecr describe-image-scan-findings --repository-name ${project}/$JOB_BASE_NAME --image-id imageTag=${appVersion}  --region ${region} --query "imageScanFindings.findings" """, returnStdout: true).trim()
 
-            echo "Scan Results: ${scanResults}"
-            if (scanResults.contains("HIGH")) {
-                currentBuild.result = 'FAILURE'
-                error("High vulnerabilities found!")
+                echo "Scan Results: ${scanResults}"
+                if (scanResults.contains("HIGH")) {
+                    currentBuild.result = 'FAILURE'
+                    error("High vulnerabilities found!")
+                }
             }
         }
     }
--- vars/nodeJs.groovy
+++ vars/nodeJs.groovy
@@ -1,24 +1,24 @@
 def call(config) {
-    //common.checkout(config)                    // Checkout code
+    common.checkout(config)                    // Checkout code
     
-    //common.readVersion("nodejs")         // Read the version from package.json
+    common.readVersion("nodejs")         // Read the version from package.json
     
-    //common.codeQuality()                 // Run SonarQube code analysis
-    //common.qualityGate()                 // Quality Gate check
+    common.codeQuality()                 // Run SonarQube code analysis
+    common.qualityGate()                 // Quality Gate check
     
-    //common.build("nodejs")               // Create zip of the build artifacts
-    //common.nexusUpload()
+    common.build("nodejs")               // Create zip of the build artifacts
+    common.nexusUpload()
 
-    //common.installDependencies("nodejs") // Install dependencies for Node.js
-    //common.dependencyCheck()             // OWASP dependency check
-    //common.snykCheck()                   // Run Snyk for vulnerabilities
+    common.installDependencies("nodejs") // Install dependencies for Node.js
+    common.dependencyCheck()             // OWASP dependency check
+    common.snykCheck()                   // Run Snyk for vulnerabilities
     
-    //common.trivyfsScan()                 // Run Trivy filesystem scan
-    //common.npmbuild("nodejs")            // Build the Node.js project
+    common.trivyfsScan()                 // Run Trivy filesystem scan
+    common.npmbuild("nodejs")            // Build the Node.js project
     
-    //common.dockerBuild()                 // Build Docker image
-    //common.dockerScout()                 // Run Docker Scout for vulnerabilities
-    //common.trivyImageScan()              // Run Trivy image scan
+    common.dockerBuild()                 // Build Docker image
+    common.dockerScout()                 // Run Docker Scout for vulnerabilities
+    common.trivyImageScan()              // Run Trivy image scan
     common.pushImagetoDockerhub()        // Push Docker image to Docker Hub
     common.pushImagetoAWSECR()           // Push Docker image to AWS ECR
 }
